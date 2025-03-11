8b090edbd01baf18f8620e9f721ffc894596ff49
Mintu Sikder <mintusikder15@gmail.com>
2025-03-11 23:22:57 +0600

    added display category

--- script/index.js
+++ script/index.js
@@ -1,12 +1,25 @@
 function loadCategories() {
   //face
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
-  .then(res => res.json())
-  .then(data => displayCategories(data.categories))
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.categories));
 }
 
-function displayCategories(categories){
-        console.log(categories)
-}
 
+
+function displayCategories(categories) {
+  //get the container
+  const container = document.getElementById("category-container");
+  //loop array of object
+  for (let cate of categories) {
+    //create element
+    const cateDiv = document.createElement("div");
+
+    cateDiv.innerHTML = `
+          <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cate.category}</button>
+          `;
+    // append element
+    container.appendChild(cateDiv);
+  }
+}
 loadCategories();
