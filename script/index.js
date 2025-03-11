36aeed454640753add987a47c5ccdc1d03815a2e
Mintu Sikder <mintusikder15@gmail.com>
2025-03-11 23:44:52 +0600

    display card done

--- script/index.js
+++ script/index.js
@@ -5,8 +5,14 @@ function loadCategories() {
     .then((data) => displayCategories(data.categories));
 }
 
+//load video
+function loadVideo() {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => displayVideo(data.videos));
+}
 
-
+//displayCategories
 function displayCategories(categories) {
   //get the container
   const container = document.getElementById("category-container");
@@ -22,4 +28,35 @@ function displayCategories(categories) {
     container.appendChild(cateDiv);
   }
 }
+
+//display video
+const displayVideo = (videos) => {
+  const videoContainer = document.getElementById("video-container")
+
+  videos.forEach(video =>{
+    console.log(video)
+     const videoCard = document.createElement("div")
+     videoCard.innerHTML =`
+  <div class="card bg-base-100  shadow-sm">
+  <figure>
+    <img class ="w-96 h-54"
+      src="${video.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+     
+     `
+     videoContainer.appendChild(videoCard)
+  })
+   
+
+};
 loadCategories();
+loadVideo();
